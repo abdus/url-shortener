f0701c010876e81f3069f11043492b04a4748253
Abdus <azad3652@gmail.com>
2018-06-22 16:22:55 +0530

    initiated

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
@@ -15,7 +14,6 @@ var app = express();
 app.engine('hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
-app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 
 var urlSchema = require('../db/url');
 var randWord = require('../rand_word/index')();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', cors(), function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.send(200);
 });
 
 /* POST home page. */
@@ -17,7 +19,7 @@ router.post('/', (req, res) => {
   });
   urlData.save((err, data) => {
     if (err) throw err;
-    return res.render('index', {data: 'Your shortlink has been created. You can find it here: https://localhost:3000/' + randWord })
+    return res.render('index', {data: 'Your shortlink has been created. You can find it here: https://localhost:3000/' + randWord });
   });
 });
 
