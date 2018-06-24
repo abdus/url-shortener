f372480e5b4bb53d17fdff14caec23dd89848315
Abdus <azad3652@gmail.com>
2018-06-24 15:03:52 +0530

    initiated

--- db/url.js
+++ db/url.js
@@ -1,7 +1,7 @@
 var config = require('./__config.json');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://' + config.userName + ':' + config.password + '@ds163700.mlab.com:63700/url-shorten');
-// mongoose.connect('mongodb://127.0.0.1/url-shorten');
+// mongoose.connect('mongodb://' + config.userName + ':' + config.password + '@ds163700.mlab.com:63700/url-shorten');
+mongoose.connect('mongodb://127.0.0.1/url-shorten');
 var Schema = mongoose.Schema;
 
 var urlSchema = Schema({
--- routes/index.js
+++ routes/index.js
@@ -22,12 +22,12 @@ router.post('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  urlSchema.findOne({urlSHORT: req.params.id}, (err, data) => {
+  urlSchema.findOne({shortURL: req.params.id}, (err, data) => {
     console.log(data);
     if (data === null) return res.render('index', {errorMsg: 'No URL found. Double Check the URL you Entered'});
     if (err)  res.render('index', {errorMsg: 'Something Went Wrong. Don\'t worry, It\'s on Server'});
     data = JSON.parse(JSON.stringify(data));
-    res.redirect(data.urlLONG);
+    res.redirect(data.longURL);
   });
 });
 
