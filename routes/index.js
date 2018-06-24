c6c47be0b75b4c50ec9cbf78eaecc0a97093afb9
Abdus <azad3652@gmail.com>
2018-06-24 14:54:58 +0530

    initiated

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,17 @@ router.post('/', (req, res) => {
   });
   urlData.save((err, data) => {
     if (err) throw err;
-    return res.render('index', {data: 'Your shortlink has been created. You can find it here: https://localhost:3000/' + randWord });
+    return res.render('index', {data: 'Your shortlink has been created. You can find it here: https://' + req.hostname + '/' + randWord });
+  });
+});
+
+router.get('/:id', (req, res) => {
+  urlSchema.findOne({urlSHORT: req.params.id}, (err, data) => {
+    console.log(data);
+    if (data === null) return res.render('index', {errorMsg: 'No URL found. Double Check the URL you Entered'});
+    if (err)  res.render('index', {errorMsg: 'Something Went Wrong. Don\'t worry, It\'s on Server'});
+    data = JSON.parse(JSON.stringify(data));
+    res.redirect(data.urlLONG);
   });
 });
 
